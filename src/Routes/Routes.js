7cdeddbccf5ca4c984b8557b5f5beb5bf9c74177
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-11 12:29:01 +0600

    header part completed in home component

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home/Home";
 import Main from '../layouts/Main'
+import Quiz from '../components/Quiz/Quiz'
+import Statistics from '../components/Statistics/Statistics'
+import Blog from "../components/Blog/Blog";
 
 export const router=createBrowserRouter([
     {
@@ -10,6 +13,18 @@ export const router=createBrowserRouter([
              {
                 path:'/',
                 element:<Home></Home>
+             },
+             {
+                path:'/quiz',
+                element:<Quiz></Quiz>
+             },
+             {
+                path:'/statistics',
+                element:<Statistics></Statistics>
+             },
+             {
+                path:'/blog',
+                element:<Blog></Blog>
              }
         ]
     }
