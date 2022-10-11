8cf5aac48441ca4848f7c0e1a51fd847fc339afe
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-11 21:32:34 +0600

    view correct answer option added through eye icon

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -3,7 +3,7 @@ import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const QuizDetails = ({ singleQuestion,handleAnswer }) => {
+const QuizDetails = ({ singleQuestion, handleAnswer }) => {
     // console.log(question);
     const { id, question, correctAnswer, options } = singleQuestion
     // console.log(singleQuestion);
@@ -17,30 +17,36 @@ const QuizDetails = ({ singleQuestion,handleAnswer }) => {
     return (
         <div className=''>
             <ToastContainer />
-        
+
             <div className='grid grid-cols-1 shadow-lg shadow-indigo-500 bg-slate-300 mix-blend-multiply bg-blend-lighten  md:w-3/5 mx-4 md:mx-auto mb-10 py-4'>
-            <div className='flex justify-center px-2 items-center py-6 mx-auto my-6 '>
-                <h4 className='text-xl  text-center px-4'> Test:  {question.replace(/(<([^>]+)>)/ig, '')} </h4>
+                <div className='flex justify-center px-2 items-center py-6 mx-auto my-6 '>
+                    <h4 className='text-xl  text-center px-4'> Test:  {question.replace(/(<([^>]+)>)/ig, '')} </h4>
 
-                <EyeIcon onClick={handleCorrectAnswer} className={`md:h-6 md:w-6 h-10 w-10 text-slate-500 ${open ? "" : "hidden"}`} />
-                <EyeSlashIcon onClick={handleCorrectAnswer} className={`md:h-6 md:w-6 h-10 w-10 text-slate-500 ${open ? "hidden" : ""}`} />
+                    <EyeIcon onClick={handleCorrectAnswer} className={`md:h-6 md:w-6 h-10 w-10 text-slate-500 ${open ? "" : "hidden"}`} />
+                    <EyeSlashIcon onClick={handleCorrectAnswer} className={`md:h-6 md:w-6 h-10 w-10 text-slate-500 ${open ? "hidden" : ""}`} />
+                </div>
+
+                <div className='grid md:grid-cols-2 md:w-3/5 gap-5 justify-center mx-auto'>
+                    {
+                        options.map((option, index) => <div key={index}
+
+                            className=" border-solid border-2 border-slate-500 p-3    rounded-md  "
+                        >
+                            <input onClick={() => handleAnswer(option, correctAnswer)} className='mx-2 cursor-pointer' type="radio" name="" id="" />
+                            {option}
+
+                        </div>
+                        )
+                    }
+                </div>
             </div>
 
-            <div className='grid md:grid-cols-2 md:w-3/5 gap-5 justify-center mx-auto'>
-                {
-                    options.map((option, index) => <div key={index}
+            <div className="alert alert-info shadow-lg md:w-2/5 flex justify-center  md:mx-auto">
+                <div className={open ? "hidden" : ""} >
 
-                        className=" border-solid border-2 border-slate-500 p-3    rounded-md  "
-                    >
-                       <input onClick={()=>handleAnswer(option,correctAnswer)} className='mx-2 cursor-pointer' type="radio" name="" id="" />
-                        {option}
-
-                    </div>
-                    )
-                }
+                    <span> {correctAnswer}</span>
+                </div>
             </div>
-            </div>
-
 
         </div>
     );
